e7674c20e17d49d3381ae08fd499f108db9256e0
Leonardo Donato <kqjut3@gmail.com>
2023-12-05 01:40:19 -0300

    ajustando codigo

--- ano2023/dia01/parte1.js
+++ ano2023/dia01/parte1.js
@@ -1,17 +1,17 @@
 import { getInput } from "../inputService.js";
 
 
-const input = await getInput(1)
+const coordinate = await getInput(1)
     .then( res => {
-        console.log(
-            res.data
+        return res.data
                 .split(/\n/gm) // separando por linha
                 .map(i => i.match(/\d/g)) // pegando todos os numeros
                 .filter(i => i) // retirando null
                 .map(i => i[0] + i.slice(-1)) //mapeando primeiro e ultimo valor
                 .map(i => parseInt(i)) // transformando em numero
-                .reduce((acc, val) => acc + val) //somando todos os valores
-        )
+                .reduce((acc, val) => acc + val) //somando todos os
     }).catch(err => {
         console.log(err);
-    });
+});
+
+console.log(coordinate);
