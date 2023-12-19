f20b326b0fcdd0827d816c400b78610e77802e63
Leonardo Donato <kqjut3@gmail.com>
2023-12-19 15:39:09 -0300

    fix: aumentando tempo de conexão

--- ano2023/inputService.js
+++ ano2023/inputService.js
@@ -1,13 +1,12 @@
 import { config } from 'dotenv';
-import https from 'https';
 import axios from 'axios';
 config({path: '../../.env'});
 
 const axiosInstance = axios.create({
     baseURL: 'https://adventofcode.com',
-    timeout: 1000,
+    timeout: 5000,
     headers: {
-        Cookie: `session=${process.env.ADVENT_CODE_KEY}`,
+        Cookie: `session=53616c7465645f5f5165f8cfe2195e94ff1ad453d2301e47e3a5ddce2e2c716550fded300bbe1c4a52c6d4797b8804fd5b740bd008ff99d58e98bf0aae3c332a`,
         Accept: 'text/html',
     }
 })
